4eabab5cc5fa7b9e5bf9e82a5afa3e3bfc38c8fe
jaguarisco <103213790+jaguarisco@users.noreply.github.com>
2024-03-13 09:27:16 -0300

    atualizando pagina

--- gustavo3a/main.js
+++ gustavo3a/main.js
@@ -1,13 +1,21 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0; i <botoes.length;1++);{
-    botoes[i].onclick = function(){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-    for(lrt j=0; j<botoes.length; j++);{
-        botoes[j].classList.remove("ativo");
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
-    
-botoes[i].classList.add("ativo");
-   }
 }
-console.log(botoes);
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-31T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
