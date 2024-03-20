b654b22b3f7060fc08b1e8a9c9c2ff2d15d012bf
jaguarisco <103213790+jaguarisco@users.noreply.github.com>
2024-03-20 09:29:49 -0300

    atualizando minha pagina

--- gustavo3a/main.js
+++ gustavo3a/main.js
@@ -15,7 +15,37 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-31T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2024-07-26T00:00:00");
+const tempoObjetivo2 = new Date("2024-08-27T00:00:00");
+const tempoObjetivo3 = new Date("2024-09-28T00:00:00");
+const tempoObjetivo4 = new Date("2024-10-29T00:00:00");
+const tempoObjetivo5 = new Date("2024-11-30T00:00:00");
+const tempoObjetivo6 = new Date("2024-12-31T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
+
+function atualizaCronometro(){
+    for(let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){}
+atualizaCronometro();
+setInterval(atualizaCronometro,1000);
